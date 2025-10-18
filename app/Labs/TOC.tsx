794bdf518a323a58253631c2968b441678969d31
Jade Gjermundsen <jadegjermundsen@Jades-MacBook-Air.local>
2025-10-18 18:15:04 -0400

    hi

--- app/Labs/TOC.tsx
+++ app/Labs/TOC.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Nav } from "react-bootstrap";
+import { Nav, NavItem, NavLink } from "react-bootstrap";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
@@ -8,60 +8,24 @@ export default function TOC() {
 
   return (
     <Nav variant="pills">
-      <Nav.Item>
-        <Nav.Link 
-          as={Link} 
-          href="/Labs" 
-          active={pathname === "/Labs"}
-        >
-          Labs
-        </Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link 
-          as={Link} 
-          href="/Labs/Lab1" 
-          active={pathname === "/Labs/Lab1"}
-        >
-          Lab 1
-        </Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link 
-          as={Link} 
-          href="/Labs/Lab2" 
-          active={pathname === "/Labs/Lab2"}
-        >
-          Lab 2
-        </Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link 
-          as={Link} 
-          href="/Labs/Lab3" 
-          active={pathname === "/Labs/Lab3"}
-        >
-          Lab 3
-        </Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link 
-          as={Link} 
-          href="/" 
-          active={pathname === "/"}
-        >
-          Kambaz
-        </Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link 
-          href="https://github.com/jannunzi"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          My GitHub
-        </Nav.Link>
-      </Nav.Item>
+      <NavItem>
+        <NavLink href="/Labs" as={Link} className={`nav-link ${pathname.endsWith("Labs") ? "active" : ""}`}>
+          Labs </NavLink> </NavItem>
+      <NavItem>
+        <NavLink href="/Labs/Lab1" as={Link} className={`nav-link ${pathname.endsWith("Lab1") ? "active" : ""}`}>
+          Lab 1 </NavLink> </NavItem>
+      <NavItem>
+        <NavLink href="/Labs/Lab2" as={Link} className={`nav-link ${pathname.endsWith("Lab2") ? "active" : ""}`}>
+          Lab 2 </NavLink> </NavItem>
+      <NavItem>
+        <NavLink href="/Labs/Lab3" as={Link} className={`nav-link ${pathname.endsWith("Lab3") ? "active" : ""}`}>
+          Lab 3 </NavLink> </NavItem>
+      <NavItem>
+        <NavLink href="/" as={Link}>
+          Kambaz </NavLink> </NavItem>
+      <NavItem>
+        <NavLink href="https://github.com/jadegjer">My GitHub</NavLink></NavItem>
     </Nav>
-  );
+  )
+ ;
 }
