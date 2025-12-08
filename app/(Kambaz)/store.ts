7739e52246edcc1597dc7e8af5263af72b14eec3
Jade Gjermundsen <jadegjermundsen@Jades-MacBook-Air.local>
2025-12-07 22:21:09 -0500

    edit store

--- app/(Kambaz)/store.ts
+++ app/(Kambaz)/store.ts
@@ -4,6 +4,7 @@ import accountReducer from "./Account/reducer";
 import assignmentsReducer from "./Courses/[cid]/Assignments/reducer";
 import coursesReducer from "./Courses/reducer";
 import enrollmentsReducer from "./Enrollments/reducer";
+import quizzesReducer from "./Courses/[cid]/Quizzes/reducer";
 
 const store = configureStore({
   reducer: {
@@ -12,6 +13,7 @@ const store = configureStore({
     assignmentsReducer,
     coursesReducer,
     enrollmentsReducer,
+    quizzesReducer,
   },
 });
 
